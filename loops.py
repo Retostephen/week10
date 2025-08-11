8648fa6cbc3a6a3bae7b00b0be1d994cca735b5b
Retostephen <retostephen2006@gmail.com>
2025-08-11 13:43:42 +0100

    Initial commit

--- loops.py
+++ loops.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
+'''
+#numbers = [1, 2, 3, 4, 5]
 
 #for number in numbers:
 #	print(number)
@@ -6,6 +7,21 @@ numbers = [1, 2, 3, 4, 5]
 #for i in range(1, 101, 3):
 #	print(f"Number {i}")
 names = ["Tom", "Jerry", "Mimi", "Val", "John"]
-for i in range(len(names)):
-	print()
+#for i in range(len(names)):
+#	print(i)
 
+
+
+#user_input = input("Enter a name: ")
+for name in names:
+	user_input = input("Enter a name: ")
+	if user_input == name:
+		print("VIP")
+	else:
+		print("Regular")
+'''
+
+for i in range (1, 101):
+	total = i * i
+	print(f"{i} * {i} = {total}")
+		
