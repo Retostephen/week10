ec77761575c22f41dac6c7483c714443c9d8274c
Retostephen <retostephen2006@gmail.com>
2025-08-11 13:04:15 +0100

    Initial commit

--- loops.py
+++ loops.py
@@ -1,3 +1,11 @@
 numbers = [1, 2, 3, 4, 5]
-for number in numbers:
-	print(number)
+
+#for number in numbers:
+#	print(number)
+
+#for i in range(1, 101, 3):
+#	print(f"Number {i}")
+names = ["Tom", "Jerry", "Mimi", "Val", "John"]
+for i in range(len(names)):
+	print()
+
