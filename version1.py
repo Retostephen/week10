de04c00543c96a09f2edd9de5903f9c67f7381f6
Retostephen <retostephen2006@gmail.com>
2025-08-12 10:52:15 +0100

    Understanding on conditionals

---
+++ version1.py
@@ -0,0 +1,10 @@
+number = 15
+
+if number > 20:
+	print("Number is greater")
+if number > 10:
+	print("Number is greater")
+if number > 5:
+	print("5 is less than number")
+if number > 14:
+	print("14 is less than number")
---
+++ version2.py
@@ -0,0 +1,12 @@
+number = 15
+
+if number > 20:
+	print("Number is greater")
+elif number > 10:
+	print("Number is greater")
+elif number > 5:
+	print("5 is less than number")
+elif number > 14:
+	print("14 is less than number")
+else:
+	print("Invalid Range")
