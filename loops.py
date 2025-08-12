9db1a3bc83121c64ae4e22e2122cea36f38644fb
Retostephen <retostephen2006@gmail.com>
2025-08-12 12:01:54 +0100

    Initial commit

--- loops.py
+++ loops.py
@@ -9,9 +9,9 @@
 names = ["Tom", "Jerry", "Mimi", "Val", "John"]
 #for i in range(len(names)):
 #	print(i)
+'''
 
-
-
+'''
 #user_input = input("Enter a name: ")
 for name in names:
 	user_input = input("Enter a name: ")
@@ -20,8 +20,21 @@ for name in names:
 	else:
 		print("Regular")
 '''
-
+'''
 for i in range (1, 101):
 	total = i * i
 	print(f"{i} * {i} = {total}")
-		
+'''
+
+names = ["Tom", "Jerry", "Mimi", "Val"]
+
+user_name = input("UserName\n>>>")
+flag = False
+for name in names:
+if user_name == name:
+		flag = True
+		break
+if flag == True:
+	print("VIP")
+else:
+	print("Regular")	
